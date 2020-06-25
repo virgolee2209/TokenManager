94ee97d19ceaf4bc9c835eb4d983201c358ae1d7
Duy Ha Le <ha@ultimate.net.au>
2020-06-25 10:59:41 +1000

    Add Test Project and test cases for JWTHelper

---
+++ TokenManager/TokenManager/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace TokenManager
+{
+    public static class ExtensionMethods
+    {
+        #region Claims
+        public static T GetValueFromClaim<T>(IEnumerable<Claim> claims, string type, T defaultValue = default(T))
+        {
+            T result = defaultValue;
+            Claim claim = claims.FirstOrDefault(a =>
+            a.Type.Equals(type, StringComparison.CurrentCultureIgnoreCase));
+            if (claim != null)
+            {
+                return (T)Convert.ChangeType(claim.Value, typeof(T));
+            }
+            return result;
+        }
+        #endregion
+    }
+}
